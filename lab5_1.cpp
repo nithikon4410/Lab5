a676ebfc7ba2dd6adf92c2b9b2b08338dc3ddaab
nithikon4410 <nithikon_jansanitsri@cmu.ac.th>
2020-12-15 16:33:32 +0700

    Filled out README

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    
+    int x,a=0,b=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >>x;
+    if(x!=0){
+        while(x!=0){
+            if(x%2==0){
+                a++;
+            }else{
+                b++;
+            }
+            cout << "Enter an integer: ";
+            cin >>x;
+        }
+    } 
+    cout << "#Even numbers = "<<a<<endl;
+    cout << "#Odd numbers = "<<b;
     return 0;
 }
